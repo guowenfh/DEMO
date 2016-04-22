94f372a06cdc53e3423c3e2e9ca4dc6f49547a71
guowenfh <guowen921@live.com>
2016-04-22 22:46:33 +0800

    添加文字描述

--- DataStructuresandAlgorithms/list/main.js
+++ DataStructuresandAlgorithms/list/main.js
@@ -1,11 +1,11 @@
-function List (){
+function List() {
     this.listSize = 0;
     this.pos = 0;
-    this.dataStore = [];   
+    this.dataStore = [];
     this.length = this.listSize;
 }
 //返回列表的字符串形式
-List.prototype.clear =function () {
+List.prototype.clear = function () {
     this.listSize = this.pos = 0;
     this.dataStore = [];
 };
@@ -18,17 +18,17 @@ List.prototype.getElement = function (index) {
     return this.dataStore[index];
 };
 //
-List.prototype.insert = function(){
-    
+List.prototype.insert = function () {
+
 };
 //在列表的末尾添加新元素
-List.prototype.append = function(element){
+List.prototype.append = function (element) {
     this.dataStore[listSize++] = element;
 };
 //从列表中删除元素
-List.prototype.remove = function(){
-    
+List.prototype.remove = function () {
+
 }
 var aa = new List();
-aa.dataStore = [123,324];
+aa.dataStore = [123, 324];
 // console.info(aa.listSize());
