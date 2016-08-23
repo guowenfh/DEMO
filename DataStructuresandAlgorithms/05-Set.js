02dcbd789b67154881f72d3e6076476cfc2d0516
guowenfh <chenguowen@raycloud.com>
2016-08-23 13:00:15 +0800

    并集

--- DataStructuresandAlgorithms/05-Set.js
+++ DataStructuresandAlgorithms/05-Set.js
@@ -42,19 +42,14 @@ function Set() {
         }
         return keys;
     };
+
+    this.union = function(otherSet) {
+        var unionSet = new Set();
+
+        var values = this.values();
+        for (var i = 0; i < values.length; i++) {
+            unionSet.add(values[i]);
+        }
+        return unionSet;
+    };
 }
-
-var set = new Set();
-set.add(1);
-console.log(set.values()); // 输出["1"]
-console.log(set.has(1)); // 输出true
-console.log(set.size()); // 输出1
-set.add(2);
-console.log(set.values()); // 输出["1", "2"]
-console.log(set.has(2)); // true
-console.log(set.size()); // 2
-set.remove(1);
-console.log(set.values()); // 输出["2"]
-set.remove(2);
-console.log(set.values()); // 输出[]
-
