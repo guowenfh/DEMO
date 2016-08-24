af6f5c23244781d426cf3ff73ea3b757ddd5d6af
guowenfh <chenguowen@raycloud.com>
2016-08-24 13:03:47 +0800

    交集子集

--- DataStructuresandAlgorithms/05-Set.js
+++ DataStructuresandAlgorithms/05-Set.js
@@ -52,4 +52,55 @@ function Set() {
         }
         return unionSet;
     };
+
+
+    this.intersection = function(otherSet) {
+        var intersectionSet = new Set();
+        var values = this.values();
+        for (var i = 0; i < values.length; i++) {
+            if (otherSet.has(values[i])) {
+                intersectionSet.add(values[i]);
+            }
+        }
+        return intersectionSet;
+    };
+
+    this.difference = function(otherSet) {
+        var differebceSet = new Set();
+        var values = this.values();
+        for (var i = 0; i < values.length; i++) {
+            if (!otherSet.has(values[i])) {
+                differebceSet.add(values[i]);
+            }
+        }
+        return differebceSet;
+    };
+
+    this.subset = function(otherSet) {
+        if (this.size() > otherSet.size()) {
+            return false;
+        } else {
+            var values = this.values();
+            for (var i = 0; i < values.length; i++) {
+                if (!otherSet.has(values[i])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    };
 }
+
+var setA = new Set();
+setA.add(1);
+setA.add(2);
+var setB = new Set();
+setB.add(1);
+setB.add(2);
+setB.add(3);
+var setC = new Set();
+setC.add(2);
+setC.add(3);
+setC.add(4);
+console.log(setA.subset(setB));
+console.log(setA.subset(setC));
