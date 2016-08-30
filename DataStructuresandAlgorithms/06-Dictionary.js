2477029f43a324912a9d6a53d1081409da02bc27
guowenfh <chenguowen@raycloud.com>
2016-08-30 12:41:00 +0800

    字典注释

--- DataStructuresandAlgorithms/06-Dictionary.js
+++ DataStructuresandAlgorithms/06-Dictionary.js
@@ -6,8 +6,8 @@ function Dictionary() {
 
     /**
      * 查找字典中是否有某个健
-     * @param {any} key
-     * @returns
+     * @param {any} key 键
+     * @returns {boolean} 存在返回true
      */
     this.has = function(key) {
         return key in items;
@@ -15,8 +15,8 @@ function Dictionary() {
 
     /**
      * 向字典添加新元素
-     * @param {any} key
-     * @param {any} value
+     * @param {any} key 键
+     * @param {any} value 值
      */
     this.set = function(key, value) {
         items[key] = value;
@@ -24,8 +24,8 @@ function Dictionary() {
 
     /**
      * 通过使用键值来从字典中移除键值对应的数据值
-     * @param {any} key
-     * @returns
+     * @param {any} key 键
+     * @returns {boolean} 成功返回true
      */
     this.remove = function(key) {
         if (this.has(key)) {
@@ -37,8 +37,8 @@ function Dictionary() {
     };
     /**
      * 通过键值，查找到特定的数值并返回
-     * @param {any} key
-     * @returns
+     * @param {any} key 键
+     * @returns {any} 找到返回值，否则undefined
      */
     this.get = function(key) {
         return this.has(key) ? items[key] : undefined;
@@ -46,7 +46,7 @@ function Dictionary() {
 
     /**
      * 将字典所包含的所有数值以数组形式返回
-     * @returns
+     * @returns {arrary} 返回所有值的数组
      */
     this.values = function() {
         var values = {};
@@ -65,21 +65,21 @@ function Dictionary() {
     };
     /**
      * 将这个字典所包含的元素的数量，与数组的length类似
-     * @returns
+     * @returns {number} 长度
      */
     this.size = function() {
         return Object.keys(items).length;
     };
     /**
      * 将字典所包含的所有键名以数组形式返回
-     * @returns
+     * @returns {array}键的数组
      */
     this.keys = function() {
         return Object.keys(items);
     };
     /**
      * 返回items变量
-     * @returns
+     * @returns {object} 整个字典存入的值
      */
     this.getItems = function() {
         return items;
