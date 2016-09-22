6b74692e25fbcdf945128a3c724d774433e275e4
guowenfh <chenguowen@raycloud.com>
2016-09-22 13:45:59 +0800

    排序算法

--- dataStructure/10-sort.js
+++ dataStructure/10-sort.js
@@ -1,30 +1,51 @@
 // 1.冒泡排序
 
+var arr = [];
 
+(function() {
+    for (var i = 0; i < 10; i++) {
+        arr.push(parseInt(Math.random() * 500, 10));
+    }
+})();
+console.log(arr);
 /**
  * 冒泡排序
- * @param {array} arr 待排序数组
+ * @param {array} array 待排序数组
  * @returns {array} 排序后数组
  */
-function bubbleSort(arr) {
-    if (!Array.isArray(arr)) {
-        console.error('Not is Array!');
-        return;
-    }
-    var array = arr;
-    var swap = function(index1, index2) {
-        var aux = array[index1];
-        array[index1] = array[index2];
-        array[index2] = aux;
-    };
+function bubbleSort(array) {
+    var temp;
     var length = array.length;
     for (var i = 0; i < length; i++) {
         for (var j = 0; j < length - 1 - i; j++) {
             if (array[j] > array[j + 1]) {
-                swap(j, j + 1);
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
         }
     }
     return array;
 }
-console.error(bubbleSort([12, 13, 435, 345, 123, 12, 33, 2, 4, 5, 76]))
+console.time('bubbleSort');
+console.error(bubbleSort(arr));
+console.timeEnd('bubbleSort');
+
+// 2.选择排序
+
+function selectionSort(array) {
+    var temp;
+    var indexMin;
+    var length = array.length;
+    for (var i = 0; i < length - 1, i++) {
+        indexMin = i;
+        for(var j = i;j<length;j++){
+            if(array[indexMin]>array[j]){
+                indexMin = j;
+            }
+        }
+        if(i!==indexMin){
+
+        }
+    }
+}
