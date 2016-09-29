e27355184e28f9c6f0f542cb07aa54d6ebee3d4b
guowenfh <chenguowen@raycloud.com>
2016-09-29 13:35:47 +0800

    二分法

--- dataStructure/10-sort.js
+++ dataStructure/10-sort.js
@@ -161,3 +161,32 @@ console.time('quickSort');
 console.error(quickSort(arr));
 console.timeEnd('quickSort');
 randomArr();
+
+console.log(arr);
+console.error(arr[5]);
+var test = arr[5];
+var binarySearch = function(arr, item) {
+    quickSort(arr);
+    var low = 0,
+        high = arr.length - 1,
+        mid, element;
+
+    while (low <= high) {
+        mid = Math.floor((low + high) / 2);
+        element = arr[mid];
+        if (element < item) {
+            low = mid + 1;
+        } else if (element > item) {
+            high = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+};
+
+console.time('binarySearch');
+console.error(quickSort(arr));
+console.error(binarySearch(arr, test));
+console.timeEnd('binarySearch');
+randomArr();
